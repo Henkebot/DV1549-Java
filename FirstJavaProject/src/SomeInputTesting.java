bd2c417d1b036bf4cf48b7cfcf0a1d1a183317ad
Henrik Nilsson <Henrik Nilsson>
2017-02-21 14:38:40 +0100

    Lite mer Teori och slumptal

--- FirstJavaProject/src/SomeInputTesting.java
+++ FirstJavaProject/src/SomeInputTesting.java
@@ -1,9 +1,39 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class SomeInputTesting
 {
 	public static void main(String[] args)
 	{
+		//Skapar ett Scanner objekt
+		//System.in är kursivt, vilket betyder att in är en klass tillhörighet
 		Scanner in = new Scanner(System.in);
+		
+		//Skapar ett Random object
+		Random randObject = new Random();
+		
+		System.out.print("Mata in ditt namn: ");
+		
+		//Skapar ett String objekt och initserar den med inmatnings strömmen
+		//in.next() läser till första blanka
+		//in.nextLine() Läser resten av strängen
+		//Det är samma progblem med att Retur följer med när man kör in.next(); 
+		//Man löser det med att man kör in.nextLine() en gång innan man tar emot inmatningen
+		String name = in.nextLine();
+		
+		System.out.println("Hejsan " + name);
+		
+		//Typomvandling
+		int randValue = (int) (Math.random() * 10 + 1);
+		
+		System.out.println(randValue);
+		
+		//Använder random objectet
+		randValue = randObject.nextInt(10) + 1;
+		
+		System.out.println(randValue);
+		
+		//Stänger strömmen
+		in.close();
 	}
 }

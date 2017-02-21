a49673ba66fe5315664bd1a63b365f8388bcb37f
Henrik Nilsson <henrik.n@live.se>
2017-02-21 16:53:37 +0100

    Relationer med Friend och dice

--- FirstJavaProject/src/Friend.java
+++ FirstJavaProject/src/Friend.java
@@ -6,12 +6,20 @@ public class Friend
 	private String lastName;
 	private int birthYear;
 	
+	//Relation, har ett Dice objekt
+	private Dice theDice;
+	
 	//Konstruktorer
 	public Friend(String firstName, String lastName, int birthYear)
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.birthYear = birthYear;
+		
+		//Nu har ett vänobjekt exakt en tärning
+		/*this.theDice = new Dice(6);*/
+		
+		this.theDice = null;
 	}
 	
 	public Friend(String firstName, String lastName)
@@ -48,6 +56,20 @@ public class Friend
 	public void setBirthYear(int birthYear)
 	{
 		this.birthYear = birthYear;
+	}
+	
+	public void setTheDice(Dice theDice)
+	{
+		this.theDice = theDice;
+	}
+	
+	public Dice giveDice()
+	{
+		Dice diceHolder = this.theDice;
+		this.theDice = null;
+		
+		return diceHolder;
+		
 	}
 	
 	public String toString()
